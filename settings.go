4c6004e0cf6a2b18a86cdbcbb91c0d957fd6a78c
Jon Charter <jmcharter@gmail.com>
2021-08-19 08:27:53 +0100

    add comments to settings

--- settings.go
+++ settings.go
@@ -4,15 +4,19 @@ import (
 	"golang.org/x/image/colornames"
 )
 
+// Settings for window and grid.
+// TILE_SIZE should be a factor of both width and height to result in a
+// grid that fits flush to the window.
 const (
 	WIDTH       = 1024               // Window width
 	HEIGHT      = 768                // Window height
-	TILE_SIZE   = 16                 // Size of individual grid tiles
+	TILE_SIZE   = 48                 // Size of individual grid tiles
 	GRID_WIDTH  = WIDTH / TILE_SIZE  // Grid width in tiles
 	GRID_HEIGHT = HEIGHT / TILE_SIZE // Grid height in tiles
 	TITLE       = "GoRL"             // Title displayed for window
 )
 
+// Various other settings such as colours and booleans.
 var (
 	BG_COLOUR   = colornames.Steelblue      // Window background colour
 	LINE_COLOUR = colornames.Lightsteelblue // Grid line colour
