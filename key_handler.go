81fc8d77e74435f37dfe71529d55379d967a6604
Jon Charter <jmcharter@gmail.com>
2021-08-21 16:46:48 +0100

    [ADD] randomise map when space bar is pressed

--- key_handler.go
+++ key_handler.go
@@ -12,5 +12,7 @@ func checkKeyPress(player *Entity, gm gameMap, window *pixelgl.Window) {
 		MoveAction(0, 1, player, gm)
 	case window.JustPressed(pixelgl.KeyDown):
 		MoveAction(0, -1, player, gm)
+	case window.JustPressed(pixelgl.KeySpace):
+		generateDungeon(&gm)
 	}
 }
